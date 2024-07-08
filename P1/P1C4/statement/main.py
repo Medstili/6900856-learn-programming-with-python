481e9c43f092bff1f9cfe4967e632c5ea6d39c79
Medstili <124106309+Medstili@users.noreply.github.com>
2024-07-08 23:35:38 +0100

    Update main.py

--- P1/P1C4/statement/main.py
+++ P1/P1C4/statement/main.py
@@ -1,1 +1,22 @@
-# Write your code here!
+class ToolBox:
+    tools=[]
+    def add(self,tool):
+        self.tools.append(tool)
+    def remove(self,tool):
+        self.tools.remove(tool)
+class ScrewDriver:
+        def __init__(self,size):
+             pass
+        def loosen(screw):
+            pass
+        def tighten(screw):
+             pass
+class Hammer:
+     def __init__(self,color):
+          pass
+     def paint(olor):
+          pass
+     def hammer_in(nail):
+          pass
+     def remove(nail):
+          pass# Write your code here!
